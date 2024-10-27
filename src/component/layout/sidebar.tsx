5c9d3d211276b7bd257dc0df7eb5322ea654007b
unknown <iamadelowotan@gmail.com>
2024-10-27 08:00:57 +0100

    update: sidebar ui updated

--- src/component/layout/sidebar.tsx
+++ src/component/layout/sidebar.tsx
@@ -55,44 +55,64 @@ const sidebarLinks = [
     heading: "BUSINESSES",
     links: [
       {
-        label: "Users",
-        href: "/",
-        icon: "/assets/svg/icons/user-friends 1.svg",
-      },
-      {
-        label: "Guarantors",
+        label: "Organization",
         href: "#",
-        icon: "/assets/svg/icons/users 1.svg",
+        icon: "/assets/svg/icons/briefcase 1.svg",
       },
       {
-        label: "Loans",
-        href: "#",
-        icon: "/assets/svg/icons/sack 1.svg",
-      },
-      {
-        label: "Decision Models",
-        href: "#",
-        icon: "/assets/svg/icons/handshake-regular 1.svg",
-      },
-      {
-        label: "Savings",
-        href: "#",
-        icon: "/assets/svg/icons/piggy-bank 1.svg",
-      },
-      {
-        label: "Loan Requests",
+        label: "Loan Products",
         href: "#",
         icon: "/assets/svg/icons/hand money.svg",
       },
       {
-        label: "Whitelist",
+        label: "Savings Products",
         href: "#",
-        icon: "/assets/svg/icons/user-check 1.svg",
+        icon: "/assets/svg/icons/np_bank_148501_000000 1.svg",
       },
       {
-        label: "Karma",
+        label: "Fees and Charges",
         href: "#",
-        icon: "/assets/svg/icons/user-times 1.svg",
+        icon: "/assets/svg/icons/transaction.svg",
+      },
+      {
+        label: "Services",
+        href: "#",
+        icon: "/assets/svg/icons/galaxy 1.svg",
+      },
+      {
+        label: "Service Account",
+        href: "#",
+        icon: "/assets/svg/icons/user-cog 1.svg",
+      },
+      {
+        label: "Settlements",
+        href: "#",
+        icon: "/assets/svg/icons/scroll 1.svg",
+      },
+      {
+        label: "Reports",
+        href: "#",
+        icon: "/assets/svg/icons/chart-bar 2.svg",
+      },
+    ],
+  },
+  {
+    heading: "SETTINGS",
+    links: [
+      {
+        label: "Preferences",
+        href: "#",
+        icon: "/assets/svg/icons/sliders-h 1.svg",
+      },
+      {
+        label: "Fees and pricing",
+        href: "#",
+        icon: "/assets/svg/icons/badge-percent 1.svg",
+      },
+      {
+        label: "Audit Logs",
+        href: "#",
+        icon: "/assets/svg/icons/audit.svg",
       },
     ],
   },
